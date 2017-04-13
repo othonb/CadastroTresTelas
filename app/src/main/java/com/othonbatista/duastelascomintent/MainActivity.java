f1464e08fbeb0586118e81768fde45b2c42f3ca9
Othon M N Batista <othonbatista@gmail.com>
2017-04-13 14:54:24 -0300

    Commit inicial

---
+++ app/src/main/java/com/othonbatista/duastelascomintent/MainActivity.java
@@ -0,0 +1,29 @@
+package com.othonbatista.duastelascomintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonCadastroClick (View v) {
+
+        Intent i = new Intent(this, CadastroActivity.class);
+
+        startActivity (i);
+    }
+
+    public void buttonVisualizaClick (View v) {
+
+        Intent i = new Intent(this, VisualizaDadosActivity.class);
+
+        startActivity (i);
+    }
+}
---
+++ app/src/main/java/com/othonbatista/duastelascomintent/VisualizaDadosActivity.java
@@ -0,0 +1,13 @@
+package com.othonbatista.duastelascomintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisualizaDadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualiza_dados);
+    }
+}
